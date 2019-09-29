29f31b41d54af893337fee21474d03a9b5e9569c
StijnOnline <stijnvandeijzen@gmail.com>
2019-09-29 20:45:02 +0200

    Removed FastIK package Added Mech Rotation based on forward directions of controllers and headset

---
+++ Mech VR/Assets/Project/Scripts/RotateMech.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateMech : MonoBehaviour
+{
+    public Transform head;
+    public Transform leftHand;
+    public Transform rightHand;
+
+    void Update()
+    {
+        Quaternion rotation = Quaternion.LookRotation((head.forward + leftHand.forward + rightHand.forward));
+        rotation.x *= 0.1f;
+        transform.localRotation = rotation;
+    }
+}
