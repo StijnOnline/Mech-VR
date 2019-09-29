da6bbe7eb16590401305cfed893ca9a3b3a35ccf
StijnOnline <stijnvandeijzen@gmail.com>
2019-09-29 20:13:31 +0200

    Edited SteamVR_IK script to handle Inverse Kinematics as desired (for when target is out of reach)

---
+++ Mech VR/Assets/PositionRelative.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionRelative : MonoBehaviour
+{
+    Transform parent;
+    Vector3 offset;
+
+    void Start()
+    {
+        parent = transform.parent;
+        offset = parent.position - transform.position;
+    }
+
+    void Update()
+    {
+        transform.position = parent.position - offset;
+    }
+}
