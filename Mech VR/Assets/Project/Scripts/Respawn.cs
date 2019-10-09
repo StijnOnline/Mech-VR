4ee97bf924bfde30f76fbdffabb72d93cd66535a
StijnOnline <stijnvandeijzen@gmail.com>
2019-10-09 19:46:42 +0200

    Changed Canvas prefab. Added Respawn script.

---
+++ Mech VR/Assets/Project/Scripts/Respawn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    private Vector3 startPos;
+    public float limit = 100f;
+
+    private void Start() {
+        startPos = transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        if((transform.position - startPos).magnitude > limit) {
+            transform.position = startPos;
+
+            Rigidbody rb = GetComponent<Rigidbody>();
+            if(rb) {
+                rb.velocity = Vector3.zero;
+            }
+        }        
+    }
+}
